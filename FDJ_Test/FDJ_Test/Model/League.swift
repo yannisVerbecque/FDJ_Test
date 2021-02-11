c2b94f495353c4f2861b97272a917de572029d38
Yannis Verbecque <9284080+yannisVerbecque@users.noreply.github.com>
2021-02-10 20:06:12 -0500

    Removing scene delegates methods, Adding League Model

---
+++ FDJ_Test/FDJ_Test/Model/League.swift
@@ -0,0 +1,26 @@
+//
+//  League.swift
+//  FDJ_Test
+//
+//  Created by Yannis VERBECQUE on 10/02/2021.
+//
+
+import Foundation
+
+struct League: Decodable {
+    let id: String
+    let name: String
+    let alternateName: String
+    let sport: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idLeague"
+        case name = "strLeague"
+        case alternateName = "strLeagueAlternate"
+        case sport = "strSport"
+    }
+    
+    func getNameLeagueForURL() -> String {
+        return ""
+    }
+}
